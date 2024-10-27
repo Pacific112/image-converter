5d17dc918a20e74fa7c7448e7c42311f144c1a4c
maciej.koziara <maciej.koziara@playboycontractors.com>
2024-10-27 22:01:31 +0100

    [chore] Rename supabase clients

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import UploadCard from "@/components/upload-card";
-import { createClient } from "@/lib/supabase/server";
+import { serverClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const supabase = await createClient();
+  const supabase = await serverClient();
 
   const { data, error } = await supabase.auth.getUser();
   if (error || !data?.user) {
