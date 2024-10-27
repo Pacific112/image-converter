9d21393ee6884fd9ee8db70a8f4fab3b58d39eb3
maciej.koziara <maciej.koziara@playboycontractors.com>
2024-10-27 20:54:45 +0100

    [chore] Test uploads

--- src/components/upload-card.tsx
+++ src/components/upload-card.tsx
@@ -1,22 +1,55 @@
 'use client'
 
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card"
 import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar"
 import {ImagesList} from "@/components/list/images-list";
 import {ImageMetadata} from "@/components/list/types";
 import {UploadDropzone} from "@/components/uploader/upload-dropzone";
+import {createClient} from "@supabase/supabase-js";
+
+
+const supabase = createClient(
+	process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+	process.env.NEXT_PUBLIC_SUPABASE_API_KEY || ''
+)
+
+const listImages = async (): Promise<ImageMetadata[]> => {
+	const uploadedFiles = await supabase.storage.from('test-uploads').list('', {limit: 20})
+	if (uploadedFiles.error) {
+		throw uploadedFiles.error
+	}
+
+	return uploadedFiles.data.map(i => {
+		return {
+			id: i.id,
+			name: i.name,
+			status: 'completed',
+			url: supabase.storage.from('test-uploads').getPublicUrl(i.name).data.publicUrl,
+		}
+	})
+}
 
 export default function UploadCard() {
 	const [images, setImages] = useState<ImageMetadata[]>([])
+	useEffect(() => {
+		listImages().then(setImages)
+	}, []);
 
 	const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		const newFiles = Array.from(event.target.files || []).map(file => ({
-			id: Math.random().toString(36).substr(2, 9),
-			name: file.name,
-			status: 'pending',
-			url: URL.createObjectURL(file)
-		} satisfies ImageMetadata))
+
+
+
+		const newFiles = Array.from(event.target.files || []).map(file => {
+			let imageMetadata = {
+				id: Math.random().toString(36).substr(2, 9),
+				name: file.name,
+				status: 'pending',
+				url: URL.createObjectURL(file)
+			} satisfies ImageMetadata;
+			supabase.storage.from('test-uploads').upload(imageMetadata.id, file).then(() => console.log("done"))
+			return imageMetadata;
+		})
 		setImages([...images, ...newFiles])
 	}
 
