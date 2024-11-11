46105b2c77c438ed45c2da1cf8f974300ac91f29
pacific112 <maciej.koziara@playboycontractors.com>
2024-11-11 22:38:34 +0100

    [chore] Move fullscreen to separate button

--- src/components/list/image-item.tsx
+++ src/components/list/image-item.tsx
@@ -6,14 +6,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import {
-  Check,
-  Image as ImageIcon,
-  LoaderIcon,
-  Share,
-  Upload,
-  X,
-} from "lucide-react";
+import { Fullscreen, LoaderIcon, Share, Upload, X } from "lucide-react";
 import { ImageUploadProgress } from "@/components/list/types";
 
 type Props = {
@@ -22,66 +15,66 @@ type Props = {
 
 export const ImageItem = ({ image }: Props) => {
   return (
-    <Dialog key={image.id}>
-      <DialogTrigger asChild>
-        <div className="relative aspect-square cursor-pointer flex items-center justify-center">
-          {image.status === "pending" ? (
-            <LoaderIcon className="animate-spin" />
-          ) : (
-            <img
-              src={image.url}
-              alt={image.name}
-              className="object-cover w-full h-full rounded-lg"
-            />
-          )}
-          {(image.status === "uploading" || image.status === "completed") && (
-            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 rounded-lg opacity-0 hover:opacity-100 transition-opacity">
-              <ImageIcon className="w-8 h-8 text-white" />
-            </div>
-          )}
-          <div className="absolute bottom-2 right-2 flex gap-2">
-            {image.status === "completed" && (
-              <button
-                onClick={async (e) => {
-                  e.preventDefault();
-                  const res = await fetch(image.downloadUrl);
-                  const blob = await res.blob();
+    <div className="relative aspect-square flex items-center justify-center">
+      {image.status === "pending" ? (
+        <LoaderIcon className="animate-spin" />
+      ) : (
+        <img
+          src={image.url}
+          alt={image.name}
+          className="object-cover w-full h-full rounded-lg"
+        />
+      )}
+      <div className="absolute top-2 rig"></div>
+      <div className="absolute bottom-2 right-2 flex gap-2">
+        {image.status === "completed" && (
+          <button
+            onClick={async (e) => {
+              e.preventDefault();
+              const res = await fetch(image.downloadUrl);
+              const blob = await res.blob();
 
-                  const file = new File([blob], `${image.name}.jpg`, {
-                    type: blob.type,
-                  });
-                  if (navigator.canShare({ files: [file] })) {
-                    await navigator.share({ files: [file] });
-                  }
-                }}
-                className="p-1 rounded-full bg-white"
-              >
-                <Share className="w-4 h-4" />
-              </button>
-            )}
-            <div className="p-1 rounded-full bg-white">
+              const file = new File([blob], `${image.name}.jpg`, {
+                type: blob.type,
+              });
+              if (navigator.canShare({ files: [file] })) {
+                await navigator.share({ files: [file] });
+              }
+            }}
+            className="p-1 rounded-full bg-white"
+          >
+            <Share className="w-4 h-4" />
+          </button>
+        )}
+        <Dialog key={image.id}>
+          <DialogTrigger asChild>
+            <button className="p-1 rounded-full bg-white">
               {image.status === "uploading" && (
                 <Upload className="w-4 h-4 text-yellow-500" />
               )}
               {image.status === "completed" && (
-                <Check className="w-4 h-4 text-green-500" />
+                <Fullscreen className="w-4 h-4" />
               )}
               {image.status === "failed" && (
                 <X className="w-4 h-4 text-red-500" />
               )}
+            </button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>{image.name}</DialogTitle>
+              <DialogDescription>Status: {image.status}</DialogDescription>
+            </DialogHeader>
+            <div className="mt-4">
+              <img
+                src={image.url}
+                alt={image.name}
+                className="w-full rounded-lg"
+              />
             </div>
-          </div>
-        </div>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>{image.name}</DialogTitle>
-          <DialogDescription>Status: {image.status}</DialogDescription>
-        </DialogHeader>
-        <div className="mt-4">
-          <img src={image.url} alt={image.name} className="w-full rounded-lg" />
-        </div>
-      </DialogContent>
-    </Dialog>
+          </DialogContent>
+        </Dialog>
+      </div>
+    </div>
   );
 };
