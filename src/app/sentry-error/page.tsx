d1ebe5724de5781dda82fbe6c2bbb85776fca464
pacific112 <maciej.koziara@playboycontractors.com>
2024-11-21 22:16:35 +0100

    [chore] Restore Sentry error for tests

---
+++ src/app/sentry-error/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default async function SentryError() {
+  return (
+    <button
+      type="button"
+      onClick={() => {
+        throw new Error("Sentry Frontend Error");
+      }}
+    >
+      Throw error
+    </button>
+  );
+}
